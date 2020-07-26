24374a7adc77c50caee5a4ceed70c81a2ce5ce2a
MartinKlasson <tillmartin2000@yahoo.se>
2020-07-26 13:03:53 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/fullstack_teaser/FullstackTeaserApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.fullstack_teaser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackTeaserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullstackTeaserApplication.class, args);
+    }
+
+}
